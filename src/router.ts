04868042edc95a23df319d8167a7317fbf685a11
vanderL <vnd.vander@gmail.com>
2021-11-17 17:20:29 -0300

    NpsController

--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,27 @@
-import { UserController } from "./controller/UserController"
-import { Router } from 'express'
-import { SurveysController } from "./controller/SurveyController"
-import { SendMailController } from "./controller/SendMailController"
-import { AnswerController } from "./controller/AnswerController"
+import { UserController } from "./controller/UserController";
+import { Router } from 'express';
+import { SurveysController } from "./controller/SurveyController";
+import { SendMailController } from "./controller/SendMailController";
+import { AnswerController } from "./controller/AnswerController";
+import { NpsController } from "./controller/NpsController";
 
-const router = Router()
+const router = Router();
 
-const userController = new UserController()
-const surveyController = new SurveysController()
-const sendEmailController = new SendMailController()
-const answerController = new AnswerController()
+const userController = new UserController();
+const surveyController = new SurveysController();
+const sendEmailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
-router.post('/users', userController.create)
+router.post('/users', userController.create);
 
-router.post('/surveys', surveyController.create)
-router.get('/surveys', surveyController.show)
+router.post('/surveys', surveyController.create);
+router.get('/surveys', surveyController.show);
 
-router.post('/sendEmail', sendEmailController.execute)
+router.post('/sendEmail', sendEmailController.execute);
 
-router.get("/answers/:value", answerController.execute)
+router.get("/answers/:value", answerController.execute);
 
-export { router }
+router.get("/nps/:survey_id", npsController.execute);
+
+export { router };

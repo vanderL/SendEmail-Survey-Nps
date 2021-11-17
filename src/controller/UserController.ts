b2803600d68a37986af1e76e5a5134a26d8ffd19
vanderL <vnd.vander@gmail.com>
2021-11-17 17:36:16 -0300

    Add validation in UserController

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,29 +1,47 @@
-import { json, Request, Response } from 'express'
+import { Request, Response } from 'express'
 import { getCustomRepository } from 'typeorm'
 import { UsersRepository } from '../mappings/UsersRepository'
+import * as yup from 'yup';
 
 class UserController {
     async create(req: Request, res: Response) {
-        const {name, email} = req.body
-        const usersRepository = getCustomRepository(UsersRepository)
+        const {name, email} = req.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required("Nome é obrigatorio"),
+            email: yup.string().email().required("Email é obrigatorio")
+        });
+
+        // if(!(await schema.isValid(req.body))) {
+        //     return res.status(400).json({error: "Validation Failed!"})
+        // }
+
+        try {
+           await schema.validate(req.body,{ abortEarly: false});  
+        } catch (error) {
+        return res.status(400).json({error: error});
+  
+        }
+
+        const usersRepository = getCustomRepository(UsersRepository);
         
         const userAlreadyExists = await usersRepository.findOne({
             email
-        })
+        });
         
         const user = usersRepository.create({
             name, email
-        })
+        });
 
         if (userAlreadyExists) {
             res.status(400).json({
                 error: 'User already exists! Recovery password'})
-        }
+        };
 
-        await usersRepository.save(user)
+        await usersRepository.save(user);
         
-        return res.status(201).json(user)
+        return res.status(201).json(user);
     }
 }
 
-export { UserController }
+export { UserController };
